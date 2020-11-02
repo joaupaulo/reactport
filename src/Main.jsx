87642f3e3bfcf1bfac6fcc10357d5a98a7c14abe
João Paulo <paulosantos1799@outlook.com>
2020-11-02 15:13:00 -0300

    praticando react

---
+++ src/Main.jsx
@@ -0,0 +1,78 @@
+import React from 'react'
+import './Main.css'
+
+const conteudo = () =>{
+
+return ( 
+
+<div className="conteudo"> 
+<h1> João Paulo</h1>
+
+<div className="links"> 
+<a> <h3>Home </h3></a>
+<a> <h3>Projetos </h3></a>
+</div>
+
+
+<div className="links"> 
+<i class="fab fa-linkedin-in"></i>  <i class="fab fa-github"></i> <i class="fas fa-envelope-open"></i>
+</div>
+
+
+
+<div className="sobre">
+
+<h3> Olá,tudo bem ? Eu me chamo João Paulo,sou desenvolvedor front-end,por enquanto estou me especializando em ReactJS,mas ainda irei com toda certeza expandir meus horizontes para novas bibliotecas e outras STACKS.Ademais,possuo conhecimento básico em Ui e Ux design,ou seja,consigo montar uma interface básica completa em Ui Design sem muita dificuldade,possuo habilidades em ferramentas usadas por designs,como,Figma,AdobexD e photoshop.No mais,no meu tempo livro gosto de ficar em casa assistindo series ou filmes,e treinando o front-end construindo layouts,além de resolver alguns desáfios no hackerrank e codewars!</h3>
+
+</div>
+
+
+
+<hr/>
+
+
+
+<div className="experiencia">
+<h1> Experiência </h1>
+<h3> Exército Brasileiro </h3>
+<h3> Soldado </h3>
+<h3> Manuntenção De Computadores</h3>
+<h3> Feveireiro De 2019 - Feveiro De 2020</h3>
+<h4> Realiza manuntenções basicas em computadores e realizava manuntenções em redes</h4>
+</div>
+
+
+<hr/>
+
+
+
+
+
+<div className="experienci">
+<h1> Linguas</h1>
+<h3> Exército Brasileiro </h3>
+<h3> Soldado </h3>
+<h3> Manuntenção De Computadores</h3>
+<h3> Feveireiro De 2019 - Feveiro De 2020</h3>
+<h4> Realiza manuntenções basicas em computadores e realizava manuntenções em redes</h4>
+</div>
+
+
+<hr/>
+
+<div className="experienci">
+<h1> Habilidades </h1>
+<h3> Exército Brasileiro </h3>
+<h3> Soldado </h3>
+<h3> Manuntenção De Computadores</h3>
+<h3> Feveireiro De 2019 - Feveiro De 2020</h3>
+<h4> Realiza manuntenções basicas em computadores e realizava manuntenções em redes</h4>
+</div>
+
+
+</div>
+
+)
+}
+
+export default conteudo;
