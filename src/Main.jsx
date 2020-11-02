01af3f0398dd0898d16f44cda258d511e41f43cc
João Paulo <paulosantos1799@outlook.com>
2020-11-02 18:42:30 -0300

    heheboy

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './Main.css'
+import foto from './images/carregar.png'
+
 
 const conteudo = () =>{
 
@@ -7,38 +9,68 @@ return (
 
 <div className="conteudo"> 
 <h1> João Paulo</h1>
-
 <div className="links"> 
-<a> <h3>Home </h3></a>
-<a> <h3>Projetos </h3></a>
+<div className="link1">
+<a> <h3>  Home </h3> </a> <a> <h3> Projetos  </h3> </a>  </div>
+<div> <i class="fab fa-github-square"></i>  <i class="fab fa-linkedin-in"></i>  <i class="fas fa-envelope-square"></i>  </div>
 </div>
-
-
-<div className="links"> 
-<i class="fab fa-linkedin-in"></i>  <i class="fab fa-github"></i> <i class="fas fa-envelope-open"></i>
-</div>
-
 
 
 <div className="sobre">
-
-<h3> Olá,tudo bem ? Eu me chamo João Paulo,sou desenvolvedor front-end,por enquanto estou me especializando em ReactJS,mas ainda irei com toda certeza expandir meus horizontes para novas bibliotecas e outras STACKS.Ademais,possuo conhecimento básico em Ui e Ux design,ou seja,consigo montar uma interface básica completa em Ui Design sem muita dificuldade,possuo habilidades em ferramentas usadas por designs,como,Figma,AdobexD e photoshop.No mais,no meu tempo livro gosto de ficar em casa assistindo series ou filmes,e treinando o front-end construindo layouts,além de resolver alguns desáfios no hackerrank e codewars!</h3>
-
+<div className="sobre1"> 
+<img className="imgport" src={foto}></img>
+<h3> Olá ! Eu me chamo,João Paulo,sou desenvolvedor front end,e estudante de sistemas da informação,contudo possuo conhecimento em ui e ux design podendo criar um layout basico completo.Até então,estudo apenas Front-end mas com toda certeza ao passar do tempo irei me dedicar a outras áreas como back end e devops!</h3>
+</div>
 </div>
 
 
-
 <hr/>
-
 
 
 <div className="experiencia">
-<h1> Experiência </h1>
-<h3> Exército Brasileiro </h3>
-<h3> Soldado </h3>
-<h3> Manuntenção De Computadores</h3>
-<h3> Feveireiro De 2019 - Feveiro De 2020</h3>
-<h4> Realiza manuntenções basicas em computadores e realizava manuntenções em redes</h4>
+
+<h2> Experiência De Trabalho </h2>
+
+
+<div className="postos"> 
+<h3> Exército Brasileiro</h3>
+<h3>Soldado</h3>
+<h3>Fevereiro de  2019 - Fevereiro De 2020 ( 1 ANO ) </h3>
+<h4> Além de realizar serviços comuns que são obritorias da área militar,eu trabalhava na seção de manuntenção de computadores realizando manuntenções basicas como formatações de computadores de windows para linux e vice-versa,manuntenções em redes de internet,ou seja trocava cabos e clipava além de outros serviços ligados a manuntenção de computadores.</h4>
+</div>
+</div>
+
+
+<hr/>
+
+
+<div className="experiencia ">
+
+<h2> Habilidades </h2>
+<div className="habilidades">
+<div>  
+<h3>Ferramentas Front End</h3> 
+<h4> HTML5  </h4>
+<h4> CSS3 </h4>
+<h4> SASS </h4>
+<h4>JAVASCRIPT</h4>
+</div>
+
+
+<div>  <h3> Frameworks </h3> 
+
+<h4>ReactJS</h4>
+
+</div>
+
+
+<div><h3>Ferramentas</h3> 
+
+<h4>Git e GitHub</h4>
+<h4>AdobeXd</h4>
+<h4>Photoshop</h4>
+</div>
+ </div>
 </div>
 
 
@@ -46,31 +78,34 @@ return (
 
 
 
+<div className="experiencia linguas">
 
+<h2>Linguas</h2>
 
-<div className="experienci">
-<h1> Linguas</h1>
-<h3> Exército Brasileiro </h3>
-<h3> Soldado </h3>
-<h3> Manuntenção De Computadores</h3>
-<h3> Feveireiro De 2019 - Feveiro De 2020</h3>
-<h4> Realiza manuntenções basicas em computadores e realizava manuntenções em redes</h4>
+<div> <h3> Português </h3> <h3>Nativo</h3> </div>
+
+<div> <h3>Inglês</h3>     <h3>Intermediário</h3>  </div>
+
 </div>
-
 
 <hr/>
 
-<div className="experienci">
-<h1> Habilidades </h1>
-<h3> Exército Brasileiro </h3>
-<h3> Soldado </h3>
-<h3> Manuntenção De Computadores</h3>
-<h3> Feveireiro De 2019 - Feveiro De 2020</h3>
-<h4> Realiza manuntenções basicas em computadores e realizava manuntenções em redes</h4>
-</div>
+<div className="experiencia ">
+
+
+<h2>Formação</h2>
+<div className="form"> 
+<h3>Universidade e Faculdade De Tecnologia e Ciências</h3>
+<h3>Sistemas Da Informação</h3>
+<h3> Em Andamento </h3>
 
 
 </div>
+</div>
+
+</div>
+
+
 
 )
 }
